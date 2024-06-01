554765356d76eb92d5332e8b66edb17b5fd271cd
Danh Nguyen <Danh.Nguyen@treeshake.com.au>
2024-06-01 21:26:39 +1000

    feat(theme): Format only

--- src/theme/assets/custom-product-list.js
+++ src/theme/assets/custom-product-list.js
@@ -1,11 +1,17 @@
-document.querySelectorAll('.custom_cllection_main .product_list').forEach((item) => {
-    item.addEventListener("click", function() {
-        if(item.classList.contains('opened')){
-            this.closest('.product_list_main').querySelector('.product_list_content').style.display = 'none';
-            item.classList.remove('opened');
-        }else{
-            this.closest('.product_list_main').querySelector('.product_list_content').style.display = 'block';
-            item.classList.add('opened');
-        }
+document
+  .querySelectorAll('.custom_cllection_main .product_list')
+  .forEach((item) => {
+    item.addEventListener('click', function () {
+      if (item.classList.contains('opened')) {
+        this.closest('.product_list_main').querySelector(
+          '.product_list_content',
+        ).style.display = 'none';
+        item.classList.remove('opened');
+      } else {
+        this.closest('.product_list_main').querySelector(
+          '.product_list_content',
+        ).style.display = 'block';
+        item.classList.add('opened');
+      }
     });
-});
+  });
