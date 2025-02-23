5e4254469cc3edc6087b0d937017b62d5b1274fc
Danh Nguyen <Danh.Nguyen@treeshake.com.au>
2025-02-24 00:41:02 +1100

    feat(all): WIP some inprogress work on Storefront API

--- src/react/components/Button.tsx
+++ src/react/components/Button.tsx
@@ -1,7 +1,15 @@
-import { ProductProvider, useProduct, useShop } from "@shopify/hydrogen-react";
-import { Product } from "@shopify/hydrogen-react/storefront-api-types";
-import React from "react";
-import { useLocation, useSearchParams } from "react-router";
+import { CartProvider, ProductProvider, useShop } from '@shopify/hydrogen-react';
+import { Product } from '@shopify/hydrogen-react/storefront-api-types';
+import { createStorefrontApiClient } from '@shopify/storefront-api-client';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useSearchParams } from 'react-router';
+
+const client = createStorefrontApiClient({
+  // load environment variables according to your framework and runtime
+  storeDomain: process.env.SHOPIFY_STORE_DOMAIN as string,
+  apiVersion: process.env.SHOPIFY_STOREFRONT_API_VERSION as string,
+  publicAccessToken: process.env.SHOPIFY_PUBLIC_STOREFRONT_TOKEN as string,
+});
 
 export function Button() {
   const [toggle, setToggle] = React.useState(false);
@@ -10,18 +18,49 @@ export function Button() {
   const shop = useShop();
 
   const product: Partial<Product> = {
-    handle: "round-lab-diamond-engagement-ring",
+    handle: 'round-lab-diamond-engagement-ring',
   };
-
-  console.log(location.pathname);
   return (
-    <ProductProvider data={product} initialVariantId={undefined}>
-      <DumpProduct />
-    </ProductProvider>
+    <CartProvider>
+      <ProductProvider data={product} initialVariantId={undefined}>
+        <DumpProduct id="8721874813177" />
+      </ProductProvider>
+    </CartProvider>
   );
 }
 
-function DumpProduct() {
-  const { product } = useProduct();
-  return <pre>{JSON.stringify(product)}</pre>;
+function DumpProduct({ id }: { id: string }) {
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    const cartSessionCookie = decodeURIComponent(getCookie('cart') ?? "");
+    
+  }, []);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await client.request(
+        `query Product {
+            product(handle: "round-lab-diamond-engagement-ring") {
+              title
+              variantsCount {
+                count
+              }
+            }
+        }`,
+      );
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  return product && <pre>{JSON.stringify(product, null, 2)}</pre>;
+}
+
+export function getCookie(name: string): string | null {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(';').shift() || null;
+  return null;
 }
